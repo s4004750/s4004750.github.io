eca588ee0135fff6f5930b5ba9b3f90d10e9f08c
s4004750 <s4004750@student.rmit.edu.au>
2024-04-16 15:26:12 +1000

    added week

---
+++ week 5/script.js
@@ -0,0 +1,15 @@
+//this is a comment
+
+let body=document.querySelector ("body");
+function checkWeather () {
+    //let temperature = prompt ("what is the outside temperature?");
+    //let temperature = 10;
+    let temp = document. querySelector ("temperature");
+    let temperature = temp.ariaValueMax;
+    if (temperature >= 10 && temperature < 20) {
+        console.log ("it feels cold");
+        body.style.backgroundColour = "lightblue";
+    } else if (temperature >= 20 && temperature <30) {
+        
+    }
+}
