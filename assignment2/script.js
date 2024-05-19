452e51c87cf762caad67005dd45090500eabb8b0
s4004750 <s4004750@student.rmit.edu.au>
2024-05-19 16:48:52 +1000

    LOTS

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,9 +1,11 @@
-const audio = document.querySelector("#media-player");
+const audio = document.querySelector("#custom-audio-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
+const replayBtn = document.querySelector("replay-btn");
+const replayImg = document.querySelector("replay-img");
 const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-playPauseBtn.addEventListener("click", togglePlayPause);
+audio.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
 audio.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
   if (audio.paused || audio.ended) {
@@ -14,20 +16,25 @@ function togglePlayPause() {
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
+function toggleReplay() {
+    if (audio.ended) {
+        audio.play();
+        replayImg.src = "icons8-replay-30.png";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const audio = document.replayBtn('audio');
+    const replayButton = document.replayBtn('replayButton');
+
+    replayButton.addEventListener('click', () => {
+        audio.currentTime = 0; // Reset audio to the beginning
+        audio.play(); // Play the audio
+    });
+});
+
 function updateProgressBar() {
   const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
-myImage.addEventListener("mouseover", makeItSquare);
-myImage.addEventListener("mouseout", makeIt);
-
-function makeItRound() {
-    myImage.classList.add("round");
-}
-
-function makeItSquare() {
-    myImage.classList.remove("round");
-}
-
-
