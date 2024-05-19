e5257ee825326cbbfdcb9337e908382b76491e6a
s4004750 <s4004750@student.rmit.edu.au>
2024-05-19 13:17:28 +1000

    added

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,24 +1,33 @@
-const video = document.querySelector("#custom-video-player");
+const audio = document.querySelector("#media-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
 video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
+playPauseBtn.addEventListener("click", togglePlayPause);
+audio.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
+  if (audio.paused || audio.ended) {
+    audio.play();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
   } else {
-    video.pause();
+    audio.pause();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
 function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
+  const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+myImage.addEventListener("mouseover", makeItSquare);
+myImage.addEventListener("mouseout", makeIt);
 
+function makeItRound() {
+    myImage.classList.add("round");
+}
+
+function makeItSquare() {
+    myImage.classList.remove("round");
+}
 
 
