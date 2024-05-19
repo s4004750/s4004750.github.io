34d147d7b8b11fec55b8b318da6ec05371c0a063
s4004750 <s4004750@student.rmit.edu.au>
2024-05-19 19:38:29 +1000

    LOTS

--- assignment2/script.js
+++ assignment2/script.js
@@ -16,12 +16,22 @@ function togglePlayPause() {
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
+// I wanted to keep the 'pause,play' feature becasue as its an audio based media player, this is the
+// only way in which the users can interact with the music on the webpage.
+
 function toggleReplay() {
-    if (audio.ended) {
-        audio.play();
-        replayImg.src = "icons8-replay-30.png";
-    }
+    audio.currentTime = 0;
+    audio.play()
+    // if (audio.ended) {
+    //     audio.play();
+    //     replayImg.src = "icons8-replay-30.png";
+    // }
 }
+// I had a lot of trouble with this code, I undertook many attempts to implement this function in the code. My frist attempt, was using the same strategy
+// as the play pause button however, through trial and error and asking many questions using the 'current.Time' code
+// was a better choice, as this allows for the user to replay the song at any point throughout the played song, rather than having to 
+// reload the page each time. I find that I personally would use this feature on websites and have previously done so. 
+// That was the main reason as to why I wanted to add in the 'Replay' button onto this media player. 
 
 document.addEventListener('DOMContentLoaded', () => {
     const audio = document.replayBtn('audio');
@@ -37,4 +47,7 @@ function updateProgressBar() {
   const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
 }
-// Add other functionalities here
+// I wanted to keep the progress bar because, regardless if you are studying or not, majority of people 
+// like to see where they are throughout the song. However for this circumstance, being that it is a media player,
+// for those who are wanting to relax or study, it is a small and minor movement on the screen that doesnt 
+// detract attention from whatever the user is trying to maintain.
